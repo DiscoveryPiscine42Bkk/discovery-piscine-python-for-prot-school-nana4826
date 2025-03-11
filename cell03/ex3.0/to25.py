93cce0d4889381cec64013e0a942a16131dc5b79
nana4826 <158984458+nana4826@users.noreply.github.com>
2025-03-11 10:38:38 +0700

    Add files via upload

---
+++ cell03/ex3.0/to25.py
@@ -0,0 +1,16 @@
+# to25.py
+
+def main():
+    # รับค่าตัวเลขจากผู้ใช้
+    number = int(("20"))
+    
+    
+    if number > 25:
+        print("Error")
+    else:
+        
+        for i in range(number, 25):
+            print(i)
+
+if __name__ == "__main__":
+    main()
