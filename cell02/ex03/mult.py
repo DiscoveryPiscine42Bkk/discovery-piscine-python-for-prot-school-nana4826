dbb8336431f6edeb4864dce51a8dbdadeb7bcd67
nana4826 <158984458+nana4826@users.noreply.github.com>
2025-03-11 10:25:03 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+# mult.py
+
+def main():
+
+    num1 = float((" 5.0 "))
+    num2 = float((" 5.0 "))
+    
+    result = num1 * num2
+    
+    print(f"ผลลัพธ์ของการคูณ {num1} และ {num2} คือ: {result}")
+
+if __name__ == "__main__":
+    main()
