b9461d165283f2d6b635cf921196d7b1b8ac333e
nana4826 <158984458+nana4826@users.noreply.github.com>
2025-03-11 13:34:00 +0700

    Add files via upload

---
+++ cell05/ex5.2/play_with_arrays++.py
@@ -0,0 +1,8 @@
+def play_with_arrays(arr):
+
+    filtered_arr = [x for x in arr if x > 5]
+    
+    print(filtered_arr)
+
+original_array = [2, 8, 1, 48, 6, 22, -6, 5]
+play_with_arrays(original_array)
