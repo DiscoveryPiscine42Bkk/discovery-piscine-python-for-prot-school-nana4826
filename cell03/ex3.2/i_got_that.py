f192467612be84df458e7c10306745746d69062a
nana4826 <158984458+nana4826@users.noreply.github.com>
2025-03-11 11:33:56 +0700

    Add files via upload

---
+++ cell03/ex3.2/i_got_that.py
@@ -0,0 +1,8 @@
+# i_got_that.py
+
+while True:
+    user_input = input("กรุณาพิมพ์คำตอบ (หรือพิมพ์ 'STOP' เพื่อหยุด): ")
+    if user_input == "STOP":
+        print("โปรแกรมจะหยุดการทำงานแล้ว")
+        break
+   
